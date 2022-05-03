86be84bb709066490d1d822fc4f3146bde50e06d
evanwashere <evnwashere@icloud.com>
2022-05-03 16:03:35 -0400

    js: add support for Bun.nanoseconds

--- src/time.mjs
+++ src/time.mjs
@@ -2,6 +2,25 @@ const time = (() => {
   const ceil = Math.ceil;
 
   try {
+    Bun.nanoseconds();
+
+    return {
+      now: Bun.nanoseconds,
+      diff: (a, b) => a - b,
+    };
+  } catch { }
+
+  try {
+    process.hrtime.bigint();
+    if ('Deno' in globalThis) throw 0;
+
+    return {
+      now: process.hrtime.bigint,
+      diff: (a, b) => Number(a - b),
+    };
+  } catch { }
+
+  try {
     Deno.core.opSync('op_bench_now');
 
     return {
@@ -16,16 +35,6 @@ const time = (() => {
     return {
       diff: (a, b) => a - b,
       now: () => ceil(1e6 * Deno.core.opSync('op_now')),
-    };
-  } catch { }
-
-  try {
-    process.hrtime.bigint();
-    if ('Deno' in globalThis) throw 0;
-
-    return {
-      now: process.hrtime.bigint,
-      diff: (a, b) => Number(a - b),
     };
   } catch { }
 
