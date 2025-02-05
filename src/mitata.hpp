96381586e355329587c9d7bb9a785e22982ab3ad
yaito3014 <ykakeyama3014@gmail.com>
2025-02-05 19:18:01 +0900

    qualify (u)intN_t

--- src/mitata.hpp
+++ src/mitata.hpp
@@ -15,10 +15,10 @@
 
 namespace mitata {
   typedef float f32; typedef double f64;
-  typedef int8_t i8; typedef uint8_t u8;
-  typedef int16_t i16; typedef uint16_t u16;
-  typedef int32_t i32; typedef uint32_t u32;
-  typedef int64_t i64; typedef uint64_t u64;
+  typedef std::int8_t i8; typedef std::uint8_t u8;
+  typedef std::int16_t i16; typedef std::uint16_t u16;
+  typedef std::int32_t i32; typedef std::uint32_t u32;
+  typedef std::int64_t i64; typedef std::uint64_t u64;
 
   namespace lib {
     struct k_stats {
